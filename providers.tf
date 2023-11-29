06d796ad504fe177b206d155d3ca3681c26c01ff
Sushma Sharma <108584232+Sushma78@users.noreply.github.com>
2023-11-29 20:56:37 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = "AKIAYEK4VUKU5Y6FVKOD" 
-  secret_key = "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" 
+  access_key = "my key" 
+  secret_key = "my key" 
 }
